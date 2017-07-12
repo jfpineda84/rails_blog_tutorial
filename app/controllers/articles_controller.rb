cf7304dbd55a1b63059ed746da38d79d3e61c107
Juan Pineda <jfpineda84@gmail.com>
2017-07-12 09:14:31 -0400

    updated app with - comment, authentication, and option to delete commets

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,21 @@
-class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
+class ArticlesController <  ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+ def index
+   @articles = Article.all
+ end
 
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
-  @article = Article.find(params[:id])
-
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render 'edit'
-    end
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -30,8 +28,24 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
+  def update
+    @article = Article.find(params[:id])
 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
     def article_params
       params.require(:article).permit(:title, :text)
     end
